8d3864b4b1d5f362189dff00780131edc98aa9de
hussein kuneen <kasmalacademy@gamil.com>
2025-02-10 05:40:06 +0300

    maintince webiste

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import main from "../src/assets/main.jpg";
+import "./app.css";
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="left">
+          <h1 className="time">Under Construction</h1>
+          <p className="title">
+            Our website is under construction , we are working very hard to give
+            you the best experince with this one
+          </p>
+          <p className="description">Down for maintenace</p>
+          <button className="button">Thank you</button>
+        </div>
+        <div className="right">
+          <img src={main} className="image" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
